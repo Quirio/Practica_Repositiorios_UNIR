148be102b0533b46bff2e1be2eb16e81aa5e7894
Alejandro Hernández Hernández <Quirio123@gmail.com>
2016-05-04 19:18:45 +0200

    AddConecction
    Adding conecction script

---
+++ Conecction.php
@@ -0,0 +1,12 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "123";
+
+	$conn = new mysqli($servername,$username,$password);
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully";
+?>
